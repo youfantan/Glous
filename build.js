bf4052df03b0f73e98d5b9934ba40d429880bebc
shandiankulishe <shandiankulishe@163.com>
2022-05-11 19:57:59 +0800

    modify build script

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
+const os= require('os')
 const sha=fs.readFileSync('sha').toString().slice(0,8)
-const os=fs.readFileSync('os').toString()
 const arch=fs.readFileSync('arch').toString()
 const version=fs.readFileSync('version').toString()
 const time=new Date().toUTCString()
@@ -8,7 +8,7 @@ const build_info={
     commit_id:sha,
     time:time,
     version:version,
-    os:os,
+    os:os.type()+' '+os.release(),
     arch:arch
 }
 fs.writeFileSync('src/assets/build_info.json',JSON.stringify(build_info))
